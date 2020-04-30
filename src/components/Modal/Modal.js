f9a1dea1dfc98b6e0d928160451d11b196f28489
Joel Brown <surfer_43@hotmail.com>
2020-04-30 14:30:23 -0400

    CSS animations and react-transition-group animations

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import Transition from 'react-transition-group/Transition'
 
 import './Modal.css';
 
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+const modal = (props) => {
+    console.log("(modal) what is the props: ", props);
+    const cssClasses = ['Modal'];
+
+    console.log("value of cssClasses: ", cssClasses);
+
+    return (
+        <Transition
+            mountOnEnter
+            unmountOnExit
+            in={props.show}
+            timeout={300}
+        >
+            { state => {
+                const cssClasses = ['Modal'
+                ,state === 'entering' 
+                ? 'ModalOpen' 
+                : state === 'exiting'? 'ModalClosed' : null ];
+                return (
+                    <div className={cssClasses.join(' ')}>
+                        <h1>A Modal</h1>
+                        <button className="Button" onClick={props.closed}>Dismiss</button>
+                    </div>
+                )
+            }}
+        </Transition>
+    );
+};
 
 export default modal;
