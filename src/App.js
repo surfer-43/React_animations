ba56755bc7d22a7ab6c2138ce4fa65f321a87b91
Joel Brown <surfer_43@hotmail.com>
2020-04-30 15:23:43 -0400

    CSSTransition approach added for blue square

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -9,7 +10,8 @@ import List from "./components/List/List";
 class App extends Component {
   state= {
     modalIsOpen: false,
-    showBlock: false
+    showRedBlock: false,
+    showBlueBlock: false
   }
   showModal = () => {
     this.setState({
@@ -23,17 +25,26 @@ class App extends Component {
     })
   }
 
-  showBlock = () => {
-    this.setState({showBlock: !this.state.showBlock});
+  showBlock = (evt) => {
+    console.log("this the the event fired: ", evt.target.innerHTML);
+    if(evt.target.innerHTML.includes("red") ) {
+      console.log("it;s all about red");
+      this.setState({showRedBlock: !this.state.showRedBlock});
+    }
+    if(evt.target.innerHTML.includes("blue") ) {
+      console.log("do the blue!");
+      this.setState({showBlueBlock: !this.state.showBlueBlock});
+    }
   }
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <button className="Button" onClick={this.showBlock}>Toggle</button>
+        <button className="Button" onClick={(event) => this.showBlock(event)}>Toggle red square</button>
+        <button className="Button" onClick={(event) => this.showBlock(event)}>Toggle blue square</button>
         <br />
         <Transition 
-          in={this.state.showBlock} 
+          in={this.state.showRedBlock} 
           timeout={1000}
           mountOnEnter
           unmountOnExit
@@ -58,6 +69,22 @@ class App extends Component {
           )}
         </Transition>
 
+        <CSSTransition
+          in={this.state.showBlueBlock} 
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => {console.log("onEnter callback was fired")}}
+          onEntering={() => {console.log("onEntering callback was fired")}}
+          onEntered={() => {console.log("onEntered callback was fired")}}
+          onExit={() => {console.log("onExit callback was fired")}}
+          onExiting={() => {console.log("onExiting callback was fired")}}
+          onExited={() => {console.log("onExited callback was fired")}}
+          classNames='fade-slide'
+        >
+          <div className='blueBox'/>
+
+        </CSSTransition>
         {/* <Transition
           in={this.state.modalIsOpen}
           timeout={300}
